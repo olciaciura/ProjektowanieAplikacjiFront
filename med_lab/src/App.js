5aa2e5a11a116c60cd9b5e9a395fa6a6105d0f42
jakulej <j.kulej01@gmail.com>
2023-11-11 12:51:20 +0100

    code cleaned

--- med_lab/src/App.js
+++ med_lab/src/App.js
@@ -14,20 +14,15 @@ function App() {
   const [page, setPage] = useState('main');
   const [patientData, setPatientData] = useState('');
   return (
-    // <div>
-    //   {page === 'main' && <Main page={page} setPage={setPage} setPatientData={setPatientData}/>}
-    //   {page === 'patient' && <Patient patientData={patientData}/>}
-    //   {page === 'add_patient' && <AddPatient/>}
-    //   {page === 'add_test' && <AddTest/>}
-    //   {page === 'all_tests' && <AllTests/>}
-    //   {page === 'doctor' && <Doctor/>}
-    //   {page === 'view_test_doctor' && <ViewTestDoctor/>}
-    //   {page === 'view_test_patient' && <ViewTestPatient/>}
-    // </div>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Main setPatientData={setPatientData}/>}/>
       <Route path='/patient' element ={<Patient patientData={patientData}/>}/>
+      <Route path='/add_patient' element ={<AddPatient/>}/>
+      <Route path='/all_tests' element ={<AllTests/>}/>
+      <Route path='/doctor' element ={<Doctor/>}/>
+      <Route path='/view_test_doctor' element ={<ViewTestDoctor/>}/>
+      <Route path='/view_test_patient' element ={<ViewTestPatient/>}/>
     </Routes>
   </BrowserRouter>
   )
