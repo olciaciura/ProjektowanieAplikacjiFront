14e94db9ab80ac4df9bfebf95a9884796d9c4cc4
olciaciura <olcia.ciura@gmail.com>
2023-11-11 13:09:20 +0100

    kinda ready add_patient

--- med_lab/src/components/AddPatient.jsx
+++ med_lab/src/components/AddPatient.jsx
@@ -4,15 +4,42 @@ import { useState } from "react";
 
 function AddPatient(props) {
 
-    const [data, setData] = useState({'pesel': '', 'imie': '', 'nazwisko': '', 'data-urodzenia': '', 'uwagi': ''})
+    const [data, setData] = useState({'pesel': '', 'firstName': '', 'secondName': '','adress': '', 'birthDate': '', 'information': ''})
 
     function setPesel(pesel) {
-        // var temp_data = data
-        // temp_data['pesel'] = pesel
         setData(temp_data => ({
             ...temp_data, 'pesel': pesel
         }))
     }
+    function setName(name) {
+        setData(temp_data => ({
+            ...temp_data, 'firstName': name
+        }))
+    }
+    function setSurname(surname) {
+        setData(temp_data => ({
+            ...temp_data, 'secondName': surname
+        }))
+    }
+    function setAddress(address) {
+        setData(temp_data => ({
+            ...temp_data, 'adress': address
+        }))
+    }
+    function setBirthDate(birth_date) {
+        setData(temp_data => ({
+            ...temp_data, 'birthDate': birth_date
+        }))
+    }
+    function setInformation(information){
+        setData(temp_data => ({
+            ...temp_data, 'information': information
+        }))
+    }
+
+    const handleSubmitClick =  () => {
+        addPatient(data);
+     };
 
     return (
         <div className="App-page">
@@ -22,13 +49,43 @@ function AddPatient(props) {
             <div id='app-body'>
                 <button id="return_button" onClick={e => props.setPage('main')}>Wróć do poprzedniej strony</button>
                 <form>
-                <input
+                    <input
                         type="text"
                         placeholder="PESEL"
                         value={data['pesel']}
                         onChange={(e) => setPesel(e.target.value)}
                     />
-                
+                    <input
+                        type="text"
+                        placeholder="Imie"
+                        value={data['firstName']}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="nazwisko"
+                        value={data['secondName']}
+                        onChange={(e) => setSurname(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="adres"
+                        value={data['address']}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="data urodzenia"
+                        value={data['birthDate']}
+                        onChange={(e) => setBirthDate(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="uwagi"
+                        value={data['information']}
+                        onChange={(e) => setInformation(e.target.value)}
+                    />    
+                    <button onClick={handleSubmitClick}>Dodaj pacjenta</button>            
                 </form>
             </div>
             <footer>
--- med_lab/src/utils.js
+++ med_lab/src/utils.js
@@ -21,3 +21,28 @@ export async function handleLogin (pesel, setPatientData, setPage) {
       setPage('patient');
     };
 };
+
+export async function addPatient (data) {  
+  console.log('test')
+  try {
+    const response = await fetch(`http://localhost:5000/patient`, {
+      method: 'POST',
+      body: JSON.parse(data)
+    });
+
+    if (response.ok) {
+      console.log('wchodze')
+      const data = await response.json();
+      console.log(response)
+      setPatientData(data);
+      setPage('patient');
+  } else {
+      console.error('Wystąpił błąd przy pobieraniu danych.');
+      setPage('patient');
+      console.log('err')
+  }
+  } catch (error) {
+    console.error('Wystąpił błąd:', error);
+    setPage('patient');
+  };
+};
